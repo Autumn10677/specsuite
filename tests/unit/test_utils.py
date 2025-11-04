d61d3a9bd1cd397a166ebbffae56186332fc7d5d
Autumn Stephens <aust8150@colorado.edu>
2025-11-04 15:30:13 -0700

    Initial commit of 'util' unit tests

---
+++ tests/unit/test_utils.py
@@ -0,0 +1,88 @@
+import sys
+import unittest
+import warnings
+import numpy as np
+
+sys.path.append("specsuite/")
+
+import utils  # noqa
+
+
+class TestMyUtils(unittest.TestCase):
+
+    def test_plot_image(self):
+        pass
+
+    def test_plot_spectra(self):
+        pass
+
+    def test_gaussian_profile(self):
+        self.assertEqual(
+            utils._gaussian(
+                x=["a", "b", "c"], A=1, mu=1, sigma=1,
+            ),
+            None
+        )
+
+        self.assertEqual(
+            utils._gaussian(
+                x=[1, 2, 3, 4], A="a", mu=10, sigma=10,
+            ),
+            None
+        )
+
+        self.assertEqual(
+            len(utils._gaussian(
+                    x=[1, 2, 3, 4], A=0, mu=10, sigma=10,
+                ),
+            ),
+            4
+        )
+
+        self.assertEqual(
+            len(utils._gaussian(
+                    x=[], A=0, mu=10, sigma=10,
+                ),
+            ),
+            0
+        )
+
+    def test_moffat_profile(self):
+        self.assertEqual(
+            utils._moffat(
+                x=["a", "b", "c"], A=1, mu=1, gamma=1,
+            ),
+            None
+        )
+
+        self.assertEqual(
+            utils._moffat(
+                x=[1, 2, 3, 4], A="a", mu=10, gamma=10,
+            ),
+            None
+        )
+
+        self.assertEqual(
+            len(utils._moffat(
+                    x=[1, 2, 3, 4], A=0, mu=10, gamma=10,
+                ),
+            ),
+            4
+        )
+
+        self.assertEqual(
+            len(utils._moffat(
+                    x=[], A=0, mu=10, gamma=10,
+                ),
+            ),
+            0
+        )
+
+    def test_rebin_image_columns(self):
+        pass
+
+    def test_flatfield_correction(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
