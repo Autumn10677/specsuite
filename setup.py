747e5a388fb442bf7b9c655afdc8ad0569a5d129
Autumn Stephens <aust8150@colorado.edu>
2026-04-10 09:25:52 -0600

    Fixed incorrect version + incorrect description in 'setup.py'

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name='specsuite',
-    version='0.1.0',    
-    description='A toolbox for processing data from slitless spectrographs',
+    version='1.1.2',    
+    description='A toolbox for processing data from long-slit spectrographs',
     url='https://github.com/Autumn10677/specsuite',
     author='Autumn Stephens',
     author_email='aust8150@colorado.edu',
