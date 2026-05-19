6a21bf674fb8f72e31ce2494fa66a744e27de0dd
Autumn Stephens <aust8150@colorado.edu>
2026-05-19 10:05:18 -0600

    Added sub-pixel offset / interpolation functions

--- specsuite/utils.py
+++ specsuite/utils.py
@@ -1,5 +1,6 @@
 from scipy.signal import fftconvolve
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 import numpy as np
 import subprocess
 import warnings
@@ -527,3 +528,436 @@ def convolve_to_resolution(
     y_conv = fftconvolve(y_padded, kernel, mode="same")[radius:-radius]
 
     return y_conv
+
+
+def correct_lightcurve_shifting(
+    flux: np.ndarray,
+    error: np.ndarray,
+    N_divisions: int = 1,
+    model_flux: np.ndarray = None,
+    mode: str = "median",
+    poly_order: int = 3,
+    apply_window: bool = True,
+    progress: bool = False,
+) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Attempts to model the sub-pixel offsets between exposures in
+    a spectrophotometric observation. Flux will be conserved during
+    interpolation, meaning that non-constant offsets can create
+    sudden jumps/drops in flux for pixels that are larger/smaller
+    than they initially were. For any 'invalid' values that would
+    require interpolating outside the range of the provided flux
+    array will be returned as a NaN.
+
+    Parameters:
+    -----------
+    flux :: np.ndarray
+        A 2D array of fluxes oriented so that the dimensions
+        represent (N_exposures, N_pixels).
+    error :: np.ndarray
+        A 2D array of errors for each entry in the 'flux' array.
+    N_divisions :: int
+        The number of equal chunks to split each exposure into
+        to infer the offset. Generally, large values (N > 5)
+        fail to accurately infer sub-pixel offsets due to a lack
+        of distinct features in each sub-region.
+    model_flux :: np.ndarray
+        A model for the flux present in each exposure. If 'None',
+        this defaults to the average flux at each pixel across
+        every exposure.
+    mode :: str
+        Determines how sub-pixel offset information will be
+        handled. 'median' calculates the median sub-pixel offset
+        across each sub-region. 'fit' will use a polynomial with
+        an order given by 'poly_order' to infer how offset varies
+        along the dispersion axis of your flux array.
+    poly_order :: int
+        The polynomial order to use for 'fit' mode. To prevent
+        issues with over-fitting, 'poly_order' must be less than
+        or equal to 'N_divisions'.
+    apply_window :: bool
+        Optionally disables the 'Hanning' window that is applied
+        to sub-regions before performing phase correlation.
+    progress :: bool
+        Toggles the progress bar.
+
+    Returns:
+    --------
+    interpolated_flux :: np.ndarray
+        Interpolated flux at each valid 'target_pixels' location.
+    interpolated_error :: np.ndarray
+        Propagated 1-sigma uncertainties of 'interp_flux'.
+    """
+
+    # Finds a 2D array of 'effective pixels' for each exposure
+    current_pixels = estimate_exposure_offsets(
+        flux=flux,
+        model_flux=model_flux,
+        N_divisions=N_divisions,
+        mode=mode,
+        poly_order=poly_order,
+        apply_window=apply_window,
+        progress=progress,
+    )
+
+    # Interpolates all exposures onto a single, shared pixel grid
+    interpolated_flux, interpolated_error = apply_new_pixel_grid(
+        flux=flux,
+        error=error,
+        current_pixels=current_pixels,
+        progress=progress,
+    )
+
+    return interpolated_flux, interpolated_error
+
+
+def estimate_exposure_offsets(
+    flux: np.ndarray,
+    model_flux: np.ndarray = None,
+    N_divisions: int = 1,
+    mode: str = "median",
+    poly_order: int = 3,
+    apply_window: bool = True,
+    progress: bool = False,
+) -> np.ndarray:
+    """
+    Attempts to estimate the effective pixel positions of the
+    input 'flux' array compared to the 'model_flux'. This is
+    done by performing a phase correlation of a model spectra
+    against each individual exposure. The phase correlation
+    produces an estimate of the sub-pixel offset(s) between
+    the model and a given exposure. If 'N_divisions' > 1,
+    this process is performed multiple sub-regions in every
+    single exposure.
+
+    Parameters:
+    -----------
+    flux :: np.ndarray
+        A 2D array of fluxes oriented so that the dimensions
+        represent (N_exposures, N_pixels).
+    model_flux :: np.ndarray
+        A 1D flux model to use for estimating sub-pixel offsets.
+        If no model is provided, the median across all exposures
+        will be used by default.
+    N_divisions :: int
+        The number of equal segments to split each individual
+        exposure into before estimating pixel offsets.
+    mode :: str
+        Controls how sub-pixel offset information is used. If
+        'median', the median sub-pixel offset across all sub-regions
+        is used for a given exposure. If 'fit', a polynomial is fit
+        to extracted offsets and used to infer how offsets change
+        the 'pixel axis' of the data.
+    poly_order :: int
+        The order of polynomial to fit to sub-pixel offets. Only
+        matters if 'fit' mode is used, and must be <= 'N_divisions'
+        to prevent over-fitting errors.
+    apply_window :: bool
+        Controls whether or not the 'hanning' window is used to smooth
+        out edge effects before performing phase correlation. Defaults
+        to 'True'.
+    progress :: bool
+        Toggles the progress bar.
+
+    Returns:
+    --------
+    offset_pixels :: np.ndarray
+        The 'true' pixel array that each exposure in 'flux' lies on.
+        These are defined relative to the 'model_flux', and may contain
+        values outside of the model's pixel locations.
+    """
+
+    # Prevents users from running the calculation before failing
+    assert mode in [
+        "median",
+        "fit",
+    ], f"Mode '{mode}' not supported, must be 'median' or 'fit'!"
+    if not (model_flux is None):
+        assert (
+            flux.shape[1] == model_flux.shape[0]
+        ), "'model_flux' must have the same shape as every individual 'flux' entry!"
+
+    # These checks only matter if performing a polynomial fit
+    if mode == "fit":
+        assert isinstance(poly_order, int), "'poly_order' must be an integer!"
+        assert (
+            poly_order <= N_divisions
+        ), "'poly_order' must be <= 'N_divisions' to prevent overfitting!"
+
+    # Makes later code easier to read
+    N_exposures, N_pixels = flux.shape
+
+    # Initialized here to allow in-place operations
+    bin_centers = np.full(N_divisions, np.nan)
+    offsets_array = np.zeros((N_exposures, N_divisions))
+
+    # Initializes pixel arrays
+    pixels = np.arange(N_pixels)
+    offset_pixels = np.array([pixels for _ in range(N_exposures)])
+
+    # This is can be an issue if many NaNs exist in 'flux'
+    if model_flux is None:
+        model_flux = np.nanmedian(flux, axis=0)
+
+    # Determines the size of each segment for splitting the SEDs
+    pad = len(pixels) // N_divisions
+
+    # Splits SEDs into N_divisions segments for analysis
+    for idx in tqdm(
+        range(N_divisions),
+        desc="Inferring sub-pixel offsets",
+        disable=(not progress),
+    ):
+
+        # Extracts portion of 'flux_model' needed for this sub-region
+        start, end = (idx * pad, (idx + 1) * pad)
+        bin_centers[idx] = (start + end) / 2
+        flux_reference = model_flux[start:end].copy()
+
+        # The 'hanning' window is used to mitigate FFT edge effects
+        if apply_window:
+            window = np.hanning(len(flux_reference))
+            flux_reference *= window
+        reference_freqs = np.fft.fft(flux_reference)
+
+        offsets_temp = []
+        for row in flux:
+
+            # Applies 'hanning' window to individual sub-region
+            flux_data = row.copy()[start:end].copy()
+            if apply_window:
+                flux_data *= window
+
+            # Estimates sub-pixel offsets
+            data_fft = np.fft.fft(flux_data)
+            shift = estimate_shift(reference_freqs, data_fft)
+            offsets_temp.append(-shift)
+
+        offsets_array[:, idx] = offsets_temp
+
+    # A constant offset is assigned to each exposure's pixels grid
+    if mode == "median":
+        median_offsets = np.median(offsets_array, axis=1)
+        offset_pixels = np.array(
+            [pix + o for pix, o in zip(offset_pixels, median_offsets)]
+        )
+
+    # Infers pixel offsets continuously using a polynomial fit
+    elif mode == "fit":
+
+        # Initializing with NaNs to intentionally break if 'np.polyfit' fails
+        coeffs = np.full((N_exposures, poly_order + 1), np.nan)
+
+        # Should only fail if 'np.polyfit()' does not converge
+        for idx in range(N_exposures):
+            coeffs[idx] = np.polyfit(bin_centers, offsets_array[idx], poly_order)
+            offset_pixels[idx] = offset_pixels[idx] + np.poly1d(coeffs[idx])(pixels)
+
+    return offset_pixels
+
+
+def perform_cdf_interpolation(
+    flux: np.ndarray,
+    error: np.ndarray,
+    current_pixels: np.ndarray,
+    target_pixels: np.ndarray,
+) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Flux-conserving interpolation using linear interpolation
+    of the cumulative distribution function (CDF). This function
+    assumes that x-errors are negligible and that the input
+    flux array has errors that are independent and random. Any
+    'target_pixels' that fall outside the range spanned by
+    'current_pixels' will be returned with a NaN value.
+
+    Parameters:
+    -----------
+    flux :: np.ndarray
+        The original flux values.
+    error :: np.ndarray
+        1-sigma uncertainties corresponding to `flux`.
+    current_pixels :: np.ndarray
+        The pixels locations of that the 'flux' and 'error' arrays
+        are currently sampled on.
+    target_pixels :: np.ndarray
+        The desired pixels to interpolate onto. Only locations (xd)
+        that lie within the range spanned by 'current_pixels' will
+        be calculated.
+
+    Returns:
+    --------
+    interp_flux :: np.ndarray
+        Interpolated flux at each valid 'target_pixels' location.
+    interp_error :: np.ndarray
+        Propagated 1-sigma uncertainties of 'interp_flux'.
+    """
+
+    # Ensures that all inputs are Numpy arrays of floats
+    flux = np.asarray(flux, dtype=float)
+    error = np.asarray(error, dtype=float)
+    x = np.asarray(current_pixels, dtype=float)
+    xd = np.asarray(target_pixels, dtype=float)
+
+    # Only checks flux since shape checks will trigger other mismatches
+    assert flux.ndim == 1, "All arrays must be 1-dimensional!"
+
+    # If lengths are different, interpolation will throw index errors
+    assert (
+        current_pixels.shape == flux.shape
+    ), "There is a shape mismatch between 'current_pixels' and 'flux'!"
+    assert (
+        flux.shape == error.shape
+    ), "'flux' and 'error' arrays should be the same shape!"
+
+    # Ensures that 'np.searchsorted()' will work as intended
+    assert np.all(x[:-1] <= x[1:]), "'current_pixels' is not sorted!"
+    assert np.all(xd[:-1] <= xd[1:]), "'target_pixels' is not sorted!"
+
+    # Uses fancy approximation, see their documentation!
+    dx = np.gradient(x)
+    dxd = np.gradient(xd)
+
+    # Converts flux into a non-normalizedCDF
+    cdf = np.cumsum(flux * dx)
+    cdf_var = np.cumsum((error * dx) ** 2)
+
+    # Requires sorted x-arrays, should 'mask' invalid x-values
+    idx = np.searchsorted(x, xd)
+    valid = (idx > 0) & (idx < len(x))
+
+    # Stores these in arrays for quick access later
+    idxL = idx[valid] - 1
+    idxR = idx[valid]
+    xL = x[idxL]
+    xR = x[idxR]
+
+    # Similar thing here, vectorized calculation helps later
+    wL = (xR - xd[valid]) / (xR - xL)
+    wR = (xd[valid] - xL) / (xR - xL)
+
+    # 'Invalid' values will have a NaN value
+    interp_cdf = np.full(len(xd), np.nan)
+    interp_cdf[valid] = wL * cdf[idxL] + wR * cdf[idxR]
+
+    # 'Invalid' values will also have NaN variances
+    interp_cdf_var = np.full(len(xd), np.nan)
+    interp_cdf_var[valid] = (
+        wL**2 * cdf_var[idxL] + wR**2 * cdf_var[idxR] + 2 * wL * wR * cdf_var[idxL]
+    )
+
+    # Dividing by 'dxd' should handle non-linear offsets
+    interp_flux = np.full(len(xd), np.nan)
+    interp_flux[1:] = (interp_cdf[1:] - interp_cdf[:-1]) / dxd[1:]
+    interp_flux[0] = interp_cdf[0]
+
+    # Initializes final variance array + finds indices to iterate over
+    interp_var = np.full(len(xd), np.nan)
+    valid_idx = np.where(valid)[0]
+
+    # Iterates over every 'valid' interpolated value
+    for k in range(1, len(valid_idx)):
+
+        # Used to find relevant interpolated CDF variances
+        i_curr = valid_idx[k]
+        i_prev = valid_idx[k - 1]
+
+        # Current interpolation weights/indices
+        aL, aR = idxL[k], idxR[k]
+        waL, waR = wL[k], wR[k]
+
+        # Previous interpolation weights/indices
+        bL, bR = idxL[k - 1], idxR[k - 1]
+        wbL, wbR = wL[k - 1], wR[k - 1]
+
+        # Covariance between adjacent interpolated CDF values
+        cov = (
+            waL * wbL * cdf_var[min(aL, bL)]
+            + waL * wbR * cdf_var[min(aL, bR)]
+            + waR * wbL * cdf_var[min(aR, bL)]
+            + waR * wbR * cdf_var[min(aR, bR)]
+        )
+
+        # Final variance of interpolated fluxes
+        interp_var[i_curr] = (
+            interp_cdf_var[i_curr] + interp_cdf_var[i_prev] - 2 * cov
+        ) / (dxd[i_curr] ** 2)
+
+    # Edge handling
+    interp_var[0] = interp_var[1]
+
+    # Converts variance into standard deviations
+    interp_error = np.sqrt(interp_var)
+
+    return interp_flux, interp_error
+
+
+def apply_new_pixel_grid(
+    flux: np.ndarray,
+    error: np.ndarray,
+    current_pixels: np.ndarray,
+    progress: bool = False,
+) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Corrects for pixel offsets between individual exposures
+    by interpolating every exposure onto the same sub-pixel
+    grid. This is done by linearly interpolating on the
+    unnormalized cummulative distribution function of each
+    exposure.
+
+    Parameters:
+    -----------
+    flux :: np.ndarray
+        A 2D array of fluxes oriented so that the dimensions
+        represent (N_exposures, N_pixels).
+    error :: np.ndarray
+        A 2D array of 1-sigma uncertainties with the same
+        dimensions as 'flux'.
+    current_pixels :: np.ndarray
+        A 2D array containing the 'effective pixel positions'
+        of the 'flux' and 'error' arrays.
+    progress :: bool
+        Toggles the progress bar.
+
+    Returns:
+    --------
+    interpolated_flux :: np.ndarray
+        A 2D array of fluxes interpolated onto a single pixel
+        grid. May contain NaN values if any 'current_pixels'
+        cannot be linearly interpolated without extrapolating
+        outside the assumed pixel bounds.
+    interpolated_error :: np.ndarray
+        A 2D array of error-propagated errors for the
+        'interpolated_flux' array.
+    """
+
+    assert flux.ndim == 2, "'flux' array must be 2D!"
+    assert (
+        flux.shape == error.shape
+    ), "'flux' and 'error' arrays should be the same shape!"
+
+    # Makes later code easier to read
+    N_exposures, N_pixels = flux.shape
+
+    # Assumes 'desired pixels' are just [0, 1, 2, ...]
+    target_pixels = np.array(range(N_pixels))
+
+    # Allows for in-place assignment
+    interpolated_flux = np.zeros((N_exposures, N_pixels))
+    interpolated_error = np.zeros((N_exposures, N_pixels))
+
+    # Interpolates each exposure onto new pixel grid
+    for idx in tqdm(
+        range(N_exposures),
+        desc="Interpolating onto new grid",
+        disable=(not progress),
+    ):
+        f_temp, e_temp = perform_cdf_interpolation(
+            flux=flux[idx],
+            error=error[idx],
+            current_pixels=current_pixels[idx],
+            target_pixels=target_pixels,
+        )
+        interpolated_flux[idx] = f_temp
+        interpolated_error[idx] = e_temp
+
+    return interpolated_flux, interpolated_error
