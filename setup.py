af19b9de4377ef4f07af9a9b1ad43b9a30767a3b
Autumn Stephens <aust8150@colorado.edu>
2026-08-11 12:58:01 -0600

    Updated PyPi publishing workflow + files

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='specsuite',
-    version='1.1.10',    
+    version='1.2.1',    
     description='A toolbox for processing data from long-slit spectrographs',
     url='https://github.com/Autumn10677/specsuite',
     author='Autumn Stephens',
